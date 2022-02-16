f393f0dc395f78b446222c5e10b0dac6833c6825
Andrew Marsh <marshandrewg@gmail.com>
2022-02-15 22:20:36 -0500

    Added emoji to chrome tab and adding pic on discordbot

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,11 @@ import Toast from 'react-bootstrap/Toast';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import {Navbar, Nav, Form, FormControl} from 'react-bootstrap';
+import ExampleNavBar from './ExampleNavBar';
+import Image from 'react-bootstrap/Image'
+import marsh from './marsh.png'
+import discordbotpic from './discordbotpic.png'
+
 // import { Document, Page } from 'react-pdf'
 // using ES6 modules
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
@@ -18,32 +23,13 @@ const ExampleToast = ({ children }) => {
       {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
       <Toast show={show} onClose={() => toggleShow(false)}>
         <Toast.Header>
-          <strong className="mr-auto">React-Bootstrap</strong>
+          <strong className="mr-auto">Welcome!</strong>
         </Toast.Header>
         <Toast.Body>{children}</Toast.Body>
       </Toast>
     </>
   );
 };
-
-function ExampleNavBar(props) {
-  return(
-  <>
-  <Navbar bg="primary" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-light">Search</Button>
-    </Form>
-  </Navbar>
-  </>
-  )
-}
 
 const App = () => (
   <div id="parent">
@@ -58,16 +44,24 @@ const App = () => (
         <h1 className="header">Andrew Marsh</h1>
         
         <ExampleToast>
-          We now have Toasts
+          Welcome to my website, I'm Andrew Marsh: a Software Engineer based in Raleigh, NC.  
           <span role="img" aria-label="tada">
             🎉
           </span>
         </ExampleToast>
-        <Document 
-          file = "andrew-marsh-resume.pdf"
-        >
-          <Page pageNumber={1} />
-        </Document>
+        <body>
+        <p>
+          I graduated from North Carolina State University with a B.S. in Computer Science. I'm most familiar with backend (especially Python & Java) but am open to learning new things, and test out front-end tech like React on my page here!
+          </p>
+      </body>
+      </Jumbotron>
+      <Jumbotron>
+        <a href="https://github.com/roanurquhart/fridge-thought-bot">Discod Bot </a>
+        <div>
+        <img src={discordbotpic}
+        width="450" 
+        height="500"></img>
+        </div>
       </Jumbotron>
     </Container>
   </div>
